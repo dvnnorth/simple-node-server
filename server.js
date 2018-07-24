b9edefa5a5e992298b2efaf595f0a4b0047a4419
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2018-07-23 20:22:43 -0500

    Created the server code! Look at me, I'm serving! THIS IS ACTUALLY VERY EXCITING!

---
+++ server.js
@@ -0,0 +1,17 @@
+const HTTP = require(`http`);
+const strObj = require(`stringify-object`);
+
+const PORT = 8080;
+
+const REQUEST_HANDLER = (request, response) => {
+    response.end(`You're here! => ` + request.url);
+};
+
+const SERVER = HTTP.createServer(REQUEST_HANDLER)
+    .listen(PORT, (err) => {
+        if (err) {
+            return console.log(`something bad happened`, err)
+        }
+
+        console.log(`server is listening on ${PORT}`)
+    });
